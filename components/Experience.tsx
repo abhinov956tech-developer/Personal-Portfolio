d4a75665025355ff3ee6ac491bf49511d507b25b
Abhinov Dutta <abhinavdutta927@gmail.com>
2024-09-25 02:37:02 +0530

    Commit Done

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -10,20 +10,20 @@ const Experience: React.FC<{}> = () => {
     EXPLORE NOW
   </p>
   <div className="container mx-auto 2xl">
-    <div className="flex flex-col items-center bg-gray-800 bg-opacity-50 rounded-xl shadow-lg p-6 mt-5">
+    <div className="flex flex-col  bg-gray-800 bg-opacity-50 rounded-xl shadow-lg p-6 mt-5">
       <div className="flex flex-row justify-between w-full">
         <p className="text-gray-300">
           <span className="font-semibold">GSSOC&apos;24 Contributor</span>
         </p>
         <p className="text-gray-300">May-August 2024</p>
       </div>
-      <p className="text-gray-300 pt-5 text-center">
+      <p className="text-gray-300 pt-5 text-left">
         Contributed in projects, raised issues, pull requests, and<br />
         also achieved Postman API Fundamentals Student Expert.
         <br />
        
       </p>
-      <div className="flex flex-col sm:flex-row justify-center mt-5">
+      <div className="flex flex-col sm:flex-row justify-start mt-5">
         <div className="bg-transparent cursor-pointer rounded-3xl text-white py-2 px-5 border border-[#2E2E2E] mr-2 mb-2">
           Html
         </div>
